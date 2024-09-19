0344859c387eed6736e3a30daf361b1b368fd05f
m-aminalizadeh <aminalizadeh@riseup.net>
2024-09-19 17:37:27 +0330

    Add ExpenseGroupContext for managing expense groups

---
+++ src/context/ExpenseGroupContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useContext } from "react";
+
+interface ExpenseGroup {
+  name: string;
+  description: string;
+  budget: number;
+}
+
+interface ExpenseGroupContextType {
+  expenseGroups: ExpenseGroup[];
+  addExpenseGroup: (group: ExpenseGroup) => void;
+}
+
+const ExpenseGroupContext = createContext<ExpenseGroupContextType | undefined>(undefined);
+
+export const useExpenseGroupContext = () => {
+  const context = useContext(ExpenseGroupContext);
+  if (!context) {
+    throw new Error("useExpenseGroupContext must be used within an ExpenseGroupProvider");
+  }
+  return context;
+};
+
+export const ExpenseGroupProvider: React.FC = ({ children }) => {
+  const [expenseGroups, setExpenseGroups] = useState<ExpenseGroup[]>([]);
+
+  const addExpenseGroup = (group: ExpenseGroup) => {
+    setExpenseGroups((prevGroups) => [...prevGroups, group]);
+  };
+
+  return (
+    <ExpenseGroupContext.Provider value={{ expenseGroups, addExpenseGroup }}>
+      {children}
+    </ExpenseGroupContext.Provider>
+  );
+};
