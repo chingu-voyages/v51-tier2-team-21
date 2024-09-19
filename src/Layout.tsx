dc6eb5f840254ea932b099d4417b2b06f2de4bb8
m-aminalizadeh <aminalizadeh@riseup.net>
2024-09-19 14:22:12 +0330

    Refactor App component to Layout component and add ExpenseGroup

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "./components/Footer.tsx";
+import ExpenseGroup from "./pages/ExpenseGroup.tsx";
 
-function App() {
+function Layout() {
   return (
     <div>
       <Footer />
+      <ExpenseGroup />
     </div>
   );
 }
 
-export default App;
+export default Layout;
