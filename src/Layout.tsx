edb837aa8c2facbf49a6895b597608e0ebe08a81
m-aminalizadeh <aminalizadeh@riseup.net>
2024-09-19 15:22:15 +0330

    Refactor Layout component to use className instead of class for styling

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -28,8 +28,8 @@ function Layout({element}) {
           </div>
         </aside>
 
-        <div class="p-4 sm:ml-64">
-          <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+        <div className="p-4 sm:ml-64">
+          <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
               {element}
               <Footer />
             </div>
