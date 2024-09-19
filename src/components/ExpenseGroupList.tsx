5942d3ed3ab6bab6459f14fe9f92711aa84c52fe
m-aminalizadeh <aminalizadeh@riseup.net>
2024-09-19 17:37:43 +0330

    Add ExpenseGroupList component for displaying expense groups

---
+++ src/components/ExpenseGroupList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useExpenseGroupContext } from "../context/ExpenseGroupContext.tsx";
+
+const ExpenseGroupList = () => {
+  const { expenseGroups } = useExpenseGroupContext();
+
+  return (
+    <div>
+      <h1>Expense Groups</h1>
+      <ul>
+        {expenseGroups.map((group, index) => (
+          <li key={index}>
+            <h2>{group.name}</h2>
+            <p>{group.description}</p>
+            <p>Budget: ${group.budget}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ExpenseGroupList;
