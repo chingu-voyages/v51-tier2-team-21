0a64b3e9288f1710149343f3308738046ee2989a
m-aminalizadeh <aminalizadeh@riseup.net>
2024-09-19 15:22:07 +0330

    Refactor index.js to add ExpenseGroup component to react-router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import Layout from './Layout.tsx';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import './index.css';
+import ExpenseGroup from './components/ExpenseGroup.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout/>,
   },
+  {
+    path: "/create-expense-group",
+    element: <Layout element={<ExpenseGroup/>} />,
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
