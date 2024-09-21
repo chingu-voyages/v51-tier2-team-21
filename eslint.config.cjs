8fb9275ba1d724bec8c61fae067c2e87b5599bb8
m-aminalizadeh <aminalizadeh@riseup.net>
2024-09-21 15:29:33 +0330

    Update ESLint configuration to include recommended rules and plugins

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,9 +1,26 @@
-// eslint.config.js
 module.exports = [
   {
     files: ['src/**/*.{js,jsx,ts,tsx}'],
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ],
+    plugins: [
+      'react',
+      '@typescript-eslint',
+      'prettier'
+    ],
     rules: {
-      // Your ESLint rules here
+      'quotes': ['error', 'single'],
+      'prettier/prettier': 'error',
+      // other ESLint rules here
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     },
   },
 ];
