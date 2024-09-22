5e8e245be816d8c8eb0df2a5e32a7e77a60a9871
Marwan <kaka.mer1998@gmail.com>
2024-09-22 06:29:54 +0300

    feat: create TeamSection component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header.tsx";
 import HeroSection from "./components/Hero.tsx";
 import { Link } from "react-router-dom";
 import React from "react";
+import TeamSection from "./components/TeamSection.tsx";
 
 function Layout({element}) {
   return (
@@ -11,6 +12,8 @@ function Layout({element}) {
       <Header />
       <HeroSection />
       <FeaturesSection />
+      <TeamSection />
+      <Footer />
         {/* <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
           <span className="sr-only">Open sidebar</span>
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
--- src/data/DevelopersInfo.ts
+++ src/data/DevelopersInfo.ts
@@ -2,6 +2,8 @@ interface Developer {
   name: string;
   github: string;
   linkedin: string;
+  role: string;
+  image: string;
 }
 
 const DEVELOPERS: Developer[] = [
@@ -9,26 +11,36 @@ const DEVELOPERS: Developer[] = [
     name: "Mudhar",
     github: "https://github.com/muudar",
     linkedin: "https://www.linkedin.com/in/mohammed-al-obaidi-41b1a8209/",
+    role: "Software Engineer",
+    image: "https://readymadeui.com/team-1.webp",
   },
   {
     name: "Parykhan Jameel",
     github: "https://github.com/parykhan-jameel",
     linkedin: "https://linkedin.com/in/parykhan-jameel",
+    role: "Software Engineer",
+    image: "https://readymadeui.com/team-1.webp",
   },
   {
     name: "Amin Alizadeh",
     github: "https://github.com/m-aminalizadeh",
     linkedin: "https://www.linkedin.com/in/m-amin-alizadeh/",
+    role: "Software Engineer",
+    image: "https://readymadeui.com/team-1.webp",
   },
   {
     name: "Marwan Baz",
     github: "https://github.com/marwanbz",
     linkedin: "https://www.linkedin.com/in/marwanbz/",
+    role: "Software Engineer",
+    image: "https://readymadeui.com/team-1.webp",
   },
   {
     name: "Nadiia Lashtun",
     github: "https://github.com/NadiiaLashtun",
     linkedin: "https://www.linkedin.com/in/lashtun/",
+    role: "Software Engineer",
+    image: "https://readymadeui.com/team-1.webp",
   },
 ];
 
@@ -36,6 +48,8 @@ const SCRUMMASTER: Developer = {
   name: "Oluwadunsin Falore",
   github: "https://github.com/Dunsin0802",
   linkedin: "http://linkedin.com/in/oluwadunsin-falore",
+  role: "Scrum Master",
+  image: "https://readymadeui.com/team-6.webp",
 };
 
 const TEAM = { DEVELOPERS, SCRUMMASTER };
