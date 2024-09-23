9c6d7e728e135bd72f3fa233fea79581466b129b
Marwan <kaka.mer1998@gmail.com>
2024-09-23 05:49:11 +0300

    fix: fix developers  data image and role properties

--- src/data/DevelopersInfo.ts
+++ src/data/DevelopersInfo.ts
@@ -11,26 +11,36 @@ const DEVELOPERS: Developer[] = [
     name: 'Mudhar',
     github: 'https://github.com/muudar',
     linkedin: 'https://www.linkedin.com/in/mohammed-al-obaidi-41b1a8209/',
+     role: "Software Engineer",
+    image: "https://readymadeui.com/team-1.webp",
   },
   {
     name: 'Parykhan Jameel',
     github: 'https://github.com/parykhan-jameel',
     linkedin: 'https://linkedin.com/in/parykhan-jameel',
+     role: "Software Engineer",
+    image: "https://readymadeui.com/team-2.webp",
   },
   {
     name: 'Amin Alizadeh',
     github: 'https://github.com/m-aminalizadeh',
     linkedin: 'https://www.linkedin.com/in/m-amin-alizadeh/',
+     role: "Software Engineer",
+    image: "https://readymadeui.com/team-3.webp",
   },
   {
     name: 'Marwan Baz',
     github: 'https://github.com/marwanbz',
     linkedin: 'https://www.linkedin.com/in/marwanbz/',
+     role: "Software Engineer",
+    image: "https://readymadeui.com/team-4.webp",
   },
   {
     name: 'Nadiia Lashtun',
     github: 'https://github.com/NadiiaLashtun',
     linkedin: 'https://www.linkedin.com/in/lashtun/',
+     role: "Software Engineer",
+    image: "https://readymadeui.com/team-5.webp",
   },
 ];
 
@@ -38,6 +48,8 @@ const SCRUMMASTER: Developer = {
   name: 'Oluwadunsin Falore',
   github: 'https://github.com/Dunsin0802',
   linkedin: 'http://linkedin.com/in/oluwadunsin-falore',
+  role: "Scrum Master",
+  image: "https://readymadeui.com/team-6.webp",
 };
 
 const TEAM = { DEVELOPERS, SCRUMMASTER };
