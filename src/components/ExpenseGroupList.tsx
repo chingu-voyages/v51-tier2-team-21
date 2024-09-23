da95ec92fc274eb4b646887cefb4985d14a2a246
m-aminalizadeh <aminalizadeh@riseup.net>
2024-09-23 13:48:33 +0330

    Refactor ExpenseGroupList component to display expense groups in a table format

--- src/components/ExpenseGroupList.tsx
+++ src/components/ExpenseGroupList.tsx
@@ -7,16 +7,56 @@ const ExpenseGroupList = () => {
   return (
     <div>
       <h1>Expense Groups</h1>
-      {expenseGroups.length === 0 && <p>No expense groups found</p>}
-      <ul>
-        {expenseGroups.map((group, index) => (
-          <li key={index}>
-            <h2>{group.name}</h2>
-            <p>{group.description}</p>
-            <p>Budget: ${group.budget}</p>
-          </li>
-        ))}
-      </ul>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-3">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Group name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Budget
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenseGroups.length === 0 && (
+              <p className="p-3">No expense groups found</p>
+            )}
+            {expenseGroups.map((group, index) => (
+              <tr
+                key={index}
+                className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize"
+                >
+                  {group.name}
+                </th>
+                <td className="px-6 py-4 text-white">
+                  {group.description.slice(0, 30)} ...
+                </td>
+                <td className="px-6 py-4 text-white">${group.budget}</td>
+                <td className="px-6 py-4 flex justify-start items-center gap-3">
+                  <button className="border font-medium text-yellow-400 p-2">
+                    Edit
+                  </button>
+                  <button className="border font-medium text-red-700 p-2">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
